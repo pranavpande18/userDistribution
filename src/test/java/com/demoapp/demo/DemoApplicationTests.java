0f4af31a4011c544d0692492cf242e265cb16675
aviral.trivedi <trivedi.aviral24@gmail.com>
2020-04-12 19:47:35 +0530

    Smoke Test

--- src/test/java/com/demoapp/demo/DemoApplicationTests.java
+++ src/test/java/com/demoapp/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.demoapp.demo;
 
+import com.demoapp.demo.controller.AdminController;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	AdminController adminController;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(adminController).isNotNull();
 	}
 
 }
