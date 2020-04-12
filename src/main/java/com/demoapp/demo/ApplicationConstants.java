1254256472f5d1d6bdd5d62fdc9e5ef80aea4f28
aviral.trivedi <trivedi.aviral24@gmail.com>
2020-04-12 19:11:52 +0530

    Avoid using string literals, adding ApplicationConstants

---
+++ src/main/java/com/demoapp/demo/ApplicationConstants.java
@@ -0,0 +1,9 @@
+package com.demoapp.demo;
+
+public interface ApplicationConstants {
+    String GET_ALL_USERS = "/";
+    String GET_USER_BY_ID = "/getUser/{id}";
+    String ADD_ONE_USER = "/addUser";
+    String UPDATE_ONE_USER = "/updateUser/{id}";
+    String DELETE_ONE_USER = "/deleteUser/{id}";
+}
