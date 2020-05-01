b454a349b532ca938d48ce5a68e401560228674c
aviral.trivedi <trivedi.aviral24@gmail.com>
2020-05-01 05:55:25 +0530

    Removing unused imports in AdminControllerTest.java

--- src/test/java/com/demoapp/demo/controller/AdminControllerTest.java
+++ src/test/java/com/demoapp/demo/controller/AdminControllerTest.java
@@ -3,24 +3,15 @@ package com.demoapp.demo.controller;
 import com.demoapp.demo.model.User;
 import com.demoapp.demo.model.UserType;
 import com.demoapp.demo.repository.UserRepository;
-import com.demoapp.demo.service.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.event.annotation.BeforeTestClass;
-import org.springframework.util.Assert;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
