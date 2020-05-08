4cd0469b4087ed31fc1958a9c2680fdd65c0afc1
aviral.trivedi <trivedi.aviral24@gmail.com>
2020-05-08 20:59:33 +0530

    DPR-01 storing user data after signin

--- src/main/java/com/demoapp/demo/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/demoapp/demo/security/services/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.demoapp.demo.security.services;
 
 import com.demoapp.demo.model.User;
 import com.demoapp.demo.repository.UserRepository;
+import com.demoapp.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
@@ -15,12 +16,16 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    UserService userService;
+
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
 
+        userService.addUser(user);
         return UserDetailsImpl.build(user);
     }
 
